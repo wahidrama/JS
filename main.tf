7c002db1e312045db8fdfc68dffad47c1db783b8
wahidrama <wahidramadhan791@gmail.com>
2022-04-06 01:25:16 +0700

    updatan updatean update

--- main.tf
+++ main.tf
@@ -31,3 +31,15 @@ resource "aws.dynamodb" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
